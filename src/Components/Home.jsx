04809ac273613ca0827af99c2cb02d801cf59dd9
Harshita Phadtare <hphadtare2023@gmail.com>
2024-04-03 14:30:46 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/backpack.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Harshita Phadtare - Web Developer & UI/UX.This is a background photo of a girl with a backpack";
 
 const Home = ({ name, title }) => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/computer.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "FitFolio",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Built user-friendly tracking platform enabling users to monitor daily exercises like running, mindfulness, and walking effortlessly.",
+    url: "https://github.com/harshitaphadtare/FitFolio/settings/pages",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Movie Recommender System (Content Based) ",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Improved user engagement by 20% by analyzing movie attributes and comparing them to user viewing history to suggest relevant films.",
+    url: "https://github.com/harshitaphadtare/Moive-Recommender-System",
   },
   {
-    title: "My Resume Site",
+    title: "Hand Gesture-Controlled Dino Game",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Utilized the CV Zone Hand Tracking Module for real-time hand tracking to control the gameplay",
+    url: "https://github.com/harshitaphadtare/dino-game",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Weather App",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "App that gives live updates on the weaather condition throught the world!",
+    url: "https://github.com/harshitaphadtare/Weather-App",
   },
 ];
 
